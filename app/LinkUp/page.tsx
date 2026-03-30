ed0d01cb41d676f005b8762626269d1963931f28
Sheikh Faraz Ahmad <sheikh.faraz.ahmad.89@gmail.com>
2026-03-31 00:44:02 +0500

    Chanaged the link for linkup project

--- app/LinkUp/page.tsx
+++ app/LinkUp/page.tsx
@@ -37,7 +37,7 @@ export default function LinkUp(){
 
             buttonText="Visit Site"
 
-            liveUrl="https://link-up-chatapp.vercel.app/"
+            liveUrl="https://link-up-next-js-full-stack.vercel.app/"
         />
     );
 };
